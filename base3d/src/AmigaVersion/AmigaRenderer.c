25429e79f3e6dd5d7ed0ba0b88f69d6107df1689
Daniel Monteiro <themontyhacker@gmail.com>
2020-09-20 20:29:19 +0100

    Add special compilation mode for the Amiga CD32
    supporting 32 colours instead of just 16

--- base3d/src/AmigaVersion/AmigaRenderer.c
+++ base3d/src/AmigaVersion/AmigaRenderer.c
@@ -64,6 +64,24 @@ extern struct ExecBase *SysBase;
 struct Window *my_window;
 struct Screen *screen;
 
+#ifdef CD32
+struct NewScreen xnewscreen = {
+    0,              /* LeftEdge*/
+    0,              /* TopEdge   */
+    320,          /* Width     */
+    200,          /* Height    */
+    5,              /* Depth   */
+    0,              /* DetailPen */
+    1,              /* BlockPen */
+    0,              /* ViewModes High-resolution, Interlaced */
+    CUSTOMSCREEN,      /* Type customized screen. */
+    NULL,          /* Font */
+    "The Mistral Report", /* Title */
+    NULL,          /* Gadget */
+    NULL          /* BitMap */
+};
+#else
+
 #ifdef AGA5BPP
 struct NewScreen xnewscreen = {
     0,			  /* LeftEdge*/
@@ -96,6 +114,7 @@ struct NewScreen xnewscreen = {
         NULL,          /* Gadget */
         NULL          /* BitMap */
 };
+#endif
 #endif
 
 struct NewWindow my_new_window = {
@@ -138,6 +157,23 @@ struct RGB8 {
     uint8_t b;
 };
 
+#ifdef CD32
+uint8_t getPaletteEntry ( uint32_t origin ) {
+    uint8_t shade;
+    
+    if ( !( origin & 0xFF000000 ) ) {
+        return TRANSPARENCY_COLOR;
+    }
+    
+    shade = 0;
+    shade += ( ( ( ( ( origin & 0x0000FF )       ) << 2 ) >> 8 ) ) << 0;
+    shade += ( ( ( ( ( origin & 0x00FF00 ) >> 8  ) << 2 ) >> 8 ) ) << 2;
+    shade += ( ( ( ( ( origin & 0xFF0000 ) >> 16 ) << 1 ) >> 8 ) ) << 4;
+    
+    return shade;
+}
+#else
+
 #ifdef AGA5BPP
 uint8_t getPaletteEntry ( uint32_t origin ) {
   uint8_t shade;
@@ -171,7 +207,7 @@ uint8_t getPaletteEntry(uint32_t origin) {
 }
 
 #endif
-
+#endif
 /*
  * Code lifted (and heavily modified) from the Strife AGA port by Lantus
  * https://github.com/lantus/Strife/blob/master/i_video.c
@@ -315,7 +351,40 @@ void graphicsInit() {
         CloseLibrary((struct Library *) IntuitionBase);
         exit(0);
     }
-
+#ifdef CD32
+    SetRGB4 ( &screen->ViewPort, 0, 0, 0, 0 );
+    SetRGB4 ( &screen->ViewPort, 1, 0, 0, 64 );
+    SetRGB4 ( &screen->ViewPort, 2, 0, 0, 128 );
+    SetRGB4 ( &screen->ViewPort, 3, 0, 0, 192 );
+    SetRGB4 ( &screen->ViewPort, 4, 0, 64, 0 );
+    SetRGB4 ( &screen->ViewPort, 5, 0, 64, 64 );
+    SetRGB4 ( &screen->ViewPort, 6, 0, 64, 128 );
+    SetRGB4 ( &screen->ViewPort, 7, 0, 64, 192 );
+    SetRGB4 ( &screen->ViewPort, 8, 0, 128, 0 );
+    SetRGB4 ( &screen->ViewPort, 9, 0, 128, 64 );
+    SetRGB4 ( &screen->ViewPort, 10, 0, 128, 128 );
+    SetRGB4 ( &screen->ViewPort, 11, 0, 128, 192 );
+    SetRGB4 ( &screen->ViewPort, 12, 0, 192, 0 );
+    SetRGB4 ( &screen->ViewPort, 13, 0, 192, 64 );
+    SetRGB4 ( &screen->ViewPort, 14, 0, 192, 128 );
+    SetRGB4 ( &screen->ViewPort, 15, 0, 192, 192 );
+    SetRGB4 ( &screen->ViewPort, 16, 128, 0, 0 );
+    SetRGB4 ( &screen->ViewPort, 17, 128, 0, 64 );
+    SetRGB4 ( &screen->ViewPort, 18, 128, 0, 128 );
+    SetRGB4 ( &screen->ViewPort, 19, 128, 0, 192 );
+    SetRGB4 ( &screen->ViewPort, 20, 128, 64, 0 );
+    SetRGB4 ( &screen->ViewPort, 21, 128, 64, 64 );
+    SetRGB4 ( &screen->ViewPort, 22, 128, 64, 128 );
+    SetRGB4 ( &screen->ViewPort, 23, 128, 64, 192 );
+    SetRGB4 ( &screen->ViewPort, 24, 128, 128, 0 );
+    SetRGB4 ( &screen->ViewPort, 25, 128, 128, 64 );
+    SetRGB4 ( &screen->ViewPort, 26, 128, 128, 128 );
+    SetRGB4 ( &screen->ViewPort, 27, 128, 128, 192 );
+    SetRGB4 ( &screen->ViewPort, 28, 128, 192, 0 );
+    SetRGB4 ( &screen->ViewPort, 29, 128, 192, 64 );
+    SetRGB4 ( &screen->ViewPort, 30, 128, 192, 128 );
+    SetRGB4 ( &screen->ViewPort, 31, 128, 192, 192 );
+#else
 #ifdef AGA5BPP
 
     for (r = 0; r < 256; r += 255) {
@@ -351,6 +420,7 @@ void graphicsInit() {
         SetRGB4(&screen->ViewPort, c, palete[c].r, palete[c].g,
                 palete[c].b);
     }
+#endif
 #endif
 
     SetPointer(my_window, emptypointer, 1, 16, 0, 0);
@@ -501,15 +571,17 @@ void handleSystemEvents() {
 }
 
 void flipRenderer() {
+#ifndef CD32
 #ifdef AGA8BPP
     OwnBlitter();
-    WaitBlit();
-    c2p1x1_8_c5_bm(320,200,0,0,&framebuffer[0], my_window->RPort->BitMap);
+    c2p1x1_8_c5_bm(320, dirtyLineY1 - dirtyLineY0, 0, dirtyLineY0, &framebuffer[dirtyLineY0 * 320], my_window->RPort->BitMap);
     DisownBlitter();
 #else
     OwnBlitter();
-    WaitBlit();
-    c2p1x1_4_c5_bm(320, 200, 0, 0, &framebuffer[0], my_window->RPort->BitMap);
+    c2p1x1_4_c5_bm(320, dirtyLineY1 - dirtyLineY0, 0, dirtyLineY0, &framebuffer[dirtyLineY0 * 320], my_window->RPort->BitMap);
     DisownBlitter();
+#endif
+#else
+    WriteChunkyPixels(my_window->RPort, 0, 0, 319, 199, &framebuffer[0], 320);
 #endif
 }
